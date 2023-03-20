0bbf9d77173aa4c0254044a20129de8dc7d0d820
Daniel <dannystvincent@gmail.com>
2023-03-19 23:20:59 -0400

    Added nltk

--- train_bot.py
+++ train_bot.py
@@ -1,22 +1,31 @@
 import nltk
+nltk.download()
 from nltk.stem import WordNetLemmatizer
 import json
 import pickle
 
 
-words= []
-classes=[]
-documents=[]
-ignore_words=['?','!','.']
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!', '.']
 data_file = open('intents.json').read()
 intents = json.loads(data_file)
 
 for intent in intents['intents']:
-    for pattern in intents['patterns']:
+    for pattern in intent['patterns']:
+        # print(pattern, 'pattern', intent['patterns'], 'intent[patterns]')
         w = nltk.word_tokenize(pattern)
-        print('Token is: {}'.format(w))
-        words.extend(w)
-        documents.append((w, intent('tag')))
+        print(w)
+        # print('Token is: {}'.format(w))
+        # words.extend(w)
+        # documents.append((w, intent['tag']))
 
-
+    #
+    #     if intent['tag'] not in classes:
+    #         classes.append(intent['tag'])
+    #
+    # print('Words list is: {}'.format(words))
+    # print('Docs are: {}'.format(documents))
+    # print('classes are: {}'.format(classes))
 
