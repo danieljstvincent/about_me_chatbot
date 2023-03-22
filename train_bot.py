e82fa1202726282d45adfa35f6de111ae78a1714
Daniel <dannystvincent@gmail.com>
2023-03-22 18:29:44 -0400

    import nltk punkt and wordnet

--- train_bot.py
+++ train_bot.py
@@ -33,4 +33,5 @@ for intent in intents['intents']:
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = list(set(words))
 
-pickle
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes,open('classes.pk1', 'wb'))
